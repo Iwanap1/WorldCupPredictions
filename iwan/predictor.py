c69d5414699c07d01c14df966edb7989947465ba
Iwan Pavord <ipavord@gmail.com>
2026-06-07 09:26:06 +0100

    test

--- iwan/predictor.py
+++ iwan/predictor.py
@@ -1,15 +1,21 @@
 from typing import List, Dict
 import pandas as pd
-from predictors.score_predictor import WorldCupScorePredictor
+from iwan.predictors.goal_probability_predictor import WorldCupGoalsPredictor
 from pathlib import Path
 import pickle
 from sklearn.preprocessing import StandardScaler
-from .utils import make_tensor_from_df
+# from iwan.utils import make_tensor_from_df
 import torch
 import json
 
 
 class IwanModel:
+    TEAM_MAP = {
+        "IR Iran": "Iran",
+        "Curaçao": "Curacao",
+        "Cabo Verde": "Cape Verde"
+    }
+
     def __init__(self, model_dir: str="models/4_goals_softmax", feature_path: str="iwan/preprocessed_data/2026_featurised_squads.json", temperature=0.8):
         model_dir = Path(model_dir)
 
@@ -22,7 +28,7 @@ class IwanModel:
         with open(feature_path, "r") as f:
             self.squad_features = json.load(f)
 
-        with open(model_dir / "scaler.pkl", "rb") as f:
+        with open(model_dir / "scalers.pkl", "rb") as f:
             self.scaler = pickle.load(f)
         
 
@@ -52,13 +58,13 @@ class IwanModel:
             res = self.predict_goals(team1, team2, isknockout)
             results[row["id"]] = {
                 "team_1_score": int(res[0]),
-                "team_2_score": int(round(res[1]))
+                "team_2_score": int(res[1])
             }
         return results
 
 
     def load_model(self, model_dir):
-        model = WorldCupScorePredictor(self.cfg)
+        model = WorldCupGoalsPredictor(self.cfg)
         state_dict = torch.load(model_dir / "model.pt")
         model.load_state_dict(state_dict)
         model.eval()
@@ -66,8 +72,10 @@ class IwanModel:
     
 
     def featurise(self, team1: str, team2: str, feature_order_list: List[str], isknockout: int=0) -> List[float]:
-        team_1_features = self.squad_features[team1]
-        team_2_features = self.squad_features[team2]
+        team1_name = self.TEAM_MAP.get(team1, team1)
+        team2_name = self.TEAM_MAP.get(team2, team2)
+        team_1_features = self.squad_features[team1_name]
+        team_2_features = self.squad_features[team2_name]
         features = {}
         for f in feature_order_list:
             if f == "is_knockout":
